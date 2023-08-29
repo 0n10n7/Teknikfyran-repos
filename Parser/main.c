70330d220da3fd8d18e9b50ca1974f0548249264
0n10n7 <mikael.kokeritz@gmail.com>
2023-08-29 11:46:08 +0200

    D2 P1 påbörjat fil tolkiningen samt string to int

--- Parser/main.c
+++ Parser/main.c
@@ -6,17 +6,53 @@
 
 #define NUM_ENTRIES 14855
 
+int ConvertStringToInt(char string[],int length){
+    int returnVal=0;
+    for (int i = length;i>0;i--){
+        int multiplier = 1;
+        for (int j = 0; j<i;j++){
+            multiplier*=10;
+        }
+        returnVal+=string[i]*multiplier;
+    }
+    return returnVal;
+}
 
 int main(){
-    char words[NUM_ENTRIES][6] = { 0 };
-    char word[5];
-    int line = 0;
+    char characters[NUM_ENTRIES][20] = { 0 };
+
+    
+
+    char lineContent[5];
+    int lineNumber = 0;
 
     FILE* fptr = fopen("thrones.csv", "r");
-    while (fscanf(fptr, "%s", word) != EOF) {
-        memcpy(words[line], word, 5);
-        words[line][5] = '\0';
-        line++;
+    //Läser in filen rad för rad
+    while (fscanf(fptr, "%s", lineContent) != EOF) {
+        int index=0;
+        char source[20] = { 0 };
+        char target[20] = { 0 };
+        char weight [3];
+        for(int i = 0; i<3;i++){
+            char currentChar = lineContent[index];
+            while (currentChar != ',' || currentChar !='\0')
+            {
+                if(i==0){ //source
+                    source[index]=currentChar;
+                }
+                else if(i == 1){ //target
+                    target[index]=currentChar;
+                }
+                else if(i == 2){ //weight
+
+                }
+                index++;
+            }
+            index++;
+        }
+        memcpy(characters[lineNumber], lineContent, 5);
+        characters[lineNumber][5] = '\0'; //positionen måste ändras
+        lineNumber++;
     }
     
 
